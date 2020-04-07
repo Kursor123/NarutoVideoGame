d86246174fac290648d0d09668d4852a3eb04e5b
Abdulkadyr <buchaev.aia@phystech.edu>
2020-04-07 13:46:03 +0300

    inheritance added

--- game.py
+++ game.py
@@ -19,246 +19,28 @@ pygame.display.set_caption('Naruto Shippuden Video Game')
 pygame.mixer.music.load('sound/main_theme.wav')
 pygame.mixer.music.play(-1)
 
-
-class Hero:
-    def __init__(self, pictures, x=50, friendly=True):
+class Character:
+    def __init__(self, pictures, x, friendly, hp, speed):
+        self.pictures = pictures
         self.width = 43
-        self.height = 58
+        self.height = 58 if friendly else 43
         self.x = x
         self.y = sizeY - self.height - 15
         self.isIdle = True
         self.isLeft = False
-        self.pictures = pictures
+        self.isDead = False
         self.isAttacking = False
         self.isJump = False
-        self.jumpCount = 10
-        self.animCount = 0
-        self.attackCount = 0
-        self.attackType = 0
-        self.friendly = friendly
-        self.hp = 300
-        self.isDead = False
-        self.deadCount = 0
-        # self.underAttack = False
-        # self.uaCount = 0
-        self.dmg = 10
-        self.speed = 8
-        self.cp = 2000
-
-    def shadow_clone_jutsu(self):
-        if self.cp <= 1500:
-            return
-        amount = random.randint(1, 4)
-        self.cp //= (amount + 1)
-        global narutoImg
-        pygame.mixer.Sound('sound/shadow_clone_jutsu.wav').play()
-        for i in range(amount):
-            objects.append(Bot(narutoImg, friendly=True, x=self.x, lvl=2))
-
-    def act(self):
-        if self.isDead:
-            return
-        self.cp += 1 if self.cp < 3000 else 0
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_k]:
-            self.shadow_clone_jutsu()
-        if not self.isAttacking:
-            if keys[pygame.K_j]:
-                self.attackCount = 0
-                self.attackType = 0
-                self.isAttacking = True
-        else:
-            if (self.attackCount == 0):
-                sound['punch'].play()
-            self.attackCount += 1
-            if self.attackCount == 4:
-                for obj in objects:
-                    if obj.friendly != self.friendly and self.collide(obj):
-                        obj.damage(self.dmg * (self.attackType + 1))
-            if self.attackCount == 8:
-                if keys[pygame.K_j]:
-                    self.attackType += 1
-                    if (self.attackType == 3):
-                        self.attackType = 0
-                        self.isAttacking = False
-                    self.attackCount = 0
-                else:
-                    self.isAttacking = False
-                    self.attackCount = 0
-                    self.attackType = 0
-
-        if keys[pygame.K_a] and self.x > 5:
-            self.x -= self.speed
-            self.isLeft = True
-            self.isIdle = False
-        elif keys[pygame.K_d] and self.x < sizeX - self.width - 5:
-            self.x += self.speed
-            self.isLeft = False
-            self.isIdle = False
-        else:
-            self.isIdle = True
-        if not self.isJump:
-            if keys[pygame.K_SPACE]:
-                self.isJump = True
-        else:
-            if self.jumpCount >= -10:
-                self.y -= 2 * self.jumpCount
-                self.jumpCount -= 1
-            else:
-                self.jumpCount = 10
-                self.isJump = False
-
-    def damage(self, dmg):
-        self.hp -= dmg
-        sound['hit'].play()
-        if self.hp <= 0:
-            self.isDead = True
-
-    def collide(self, obj):
-        if obj.isDead:
-            return
-
-        def isInRect(x, y, obj):
-            return obj.x <= x <= obj.x + obj.width and obj.y <= y <= obj.y + obj.height
-
-        res = isInRect(self.x, self.y, obj)
-        res |= isInRect(self.x + self.width, self.y, obj)
-        res |= isInRect(self.x + self.width, self.y + self.height, obj)
-        res |= isInRect(self.x, self.y + self.height, obj)
-        return res
-
-    def animate(self):
-        global win
-        win.blit(self.pictures['prof'][0], (20, 20))
-        pygame.draw.rect(win, (255, 0, 0), (90, 43, 300, 10))
-        pygame.draw.rect(win, (0, 200, 255), (90, 53, self.cp // 10, 10))
-        pygame.draw.rect(win, (0, 0, 0), (90, 53, self.cp // 10, 10), 1)
-        if not self.isDead:
-            pygame.draw.rect(win, (0, 255, 0), (90, 43, self.hp, 10))
-            pygame.draw.rect(win, (0, 0, 0), (90, 43, self.hp, 10), 1)
-        pygame.draw.rect(win, (0, 0, 0), (90, 43, 300, 10), 1)
-        pygame.draw.rect(win, (0, 0, 0), (90, 53, 300, 10), 1)
-        if self.isDead:
-            win.blit(self.pictures['dead{}'.format('L' if self.isLeft else 'R')]
-                     [self.deadCount // 6], (self.x + (self.deadCount // 6) * 25,
-                                             self.y + (self.deadCount // 6) * 10))
-            if self.deadCount < 29:
-                self.deadCount += 1
-        elif self.isAttacking:
-            win.blit(self.pictures['attack{}'.format('L' if self.isLeft else 'R')]
-                     [self.attackType][self.attackCount // 2], (self.x, self.y))
-        elif self.isJump:
-            win.blit(self.pictures['jump{}'.format('L' if self.isLeft else 'R')]
-                     [(self.jumpCount + 10) // 7], (self.x, self.y))
-        elif self.isIdle:
-            win.blit(self.pictures['idle{}'.format('L' if self.isLeft else 'R')]
-                     [self.animCount // 5], (self.x, self.y))
-            self.animCount += 1
-            self.animCount %= 30
-        else:
-            win.blit(self.pictures['run{}'.format('L' if self.isLeft else 'R')]
-                     [self.animCount // 5], (self.x, self.y))
-            self.animCount += 1
-            self.animCount %= 30
-
-
-class Bot:
-    def __init__(self, pictures, friendly=False, lvl=1, x = 'nt'):
-        self.width = 43
-        self.height = 58 if friendly else 43
-        self.x = (sizeX // 2) + (2 * random.randint(0, 1) - 1) * (sizeX // 2 + 50) if x == 'nt' else x
-        self.y = sizeY - self.height - 15
-        self.isIdle = True
-        self.isLeft = False
-        self.pictures = pictures
-        self.isAttacking = False
-        self.isJump = False
-        self.jumpCount = 10
-        self.animCount = 0
-        self.attackCount = 0
-        self.attackType = 0
-        self.friendly = friendly
-        self.isTargetChosen = False
-        self.hp = 10 + 20 * lvl
-        self.isDead = False
         self.underAttack = False
         self.uaCount = 0
-        self.lvl = lvl
-        self.dmg = 5 * lvl
-        self.speed = 4 if lvl < 5 else 6
-
-    def chooseTarget(self):
-        if self.isTargetChosen:
-            if self.target.isDead:
-                self.isTargetChosen = False
-            return
-        enemies = []
-        for obj in objects:
-            if self.friendly != obj.friendly and not obj.isDead:
-                enemies.append(obj)
-        if len(enemies):
-            self.isTargetChosen = True
-            self.target = enemies[random.randint(0, len(enemies) - 1)]
-
-    def collide(self, obj):
-        if obj.isDead:
-            return
-
-        def isInRect(x, y, obj):
-            return obj.x <= x <= obj.x + obj.width and obj.y <= y <= obj.y + obj.height
-
-        res = isInRect(self.x, self.y, obj)
-        res |= isInRect(self.x + self.width, self.y, obj)
-        res |= isInRect(self.x + self.width, self.y + self.height, obj)
-        res |= isInRect(self.x, self.y + self.height, obj)
-        return res
-
-    def act(self):
-        if (self.isDead):
-            return
-        self.chooseTarget()
-        atProb = random.randint(1, 200) <= 9
-        if not self.isAttacking:
-            if self.isTargetChosen and self.collide(self.target) and atProb:
-                self.attackCount = 0
-                self.attackType = 0
-                self.isAttacking = True
-        else:
-            if (self.attackCount == 0):
-                sound['punch'].play()
-            self.attackCount += 1
-            if (self.attackCount == 4):
-                self.target.damage(self.dmg * (self.attackType + 1))
-            if self.attackCount == 8:
-                if self.isTargetChosen and self.collide(self.target):
-                    self.attackType += 1
-                    if self.attackType == 3:
-                        self.attackType = 0
-                        self.isAttacking = False
-                    self.attackCount = 0
-                else:
-                    self.isAttacking = False
-                    self.attackCount = 0
-                    self.attackType = 0
-        if not self.isTargetChosen:
-            self.isIdle = True
-            return
-
-        self.isLeft = (self.x > self.target.x)
-        self.isIdle = self.collide(self.target)
-        self.x += (-1 if self.isLeft else 1) * ((
-            self.speed + 2 * random.randint(0, 1) - 1) *
-            (0 if self.collide(self.target) else 1))
-        jumpProb = random.randint(1, 100) <= 2
-        if self.isTargetChosen and not self.isJump and jumpProb:
-            self.isJump = True
-        if self.isJump:
-            if self.jumpCount >= -10:
-                self.y -= self.jumpCount
-                self.jumpCount -= 1
-            else:
-                self.jumpCount = 10
-                self.isJump = False
+        self.jumpCount = 10
+        self.animCount = 0
+        self.attackCount = 0
+        self.deadCount = 0
+        self.attackType = 0
+        self.friendly = friendly
+        self.hp = hp
+        self.speed = speed
 
     def damage(self, dmg):
         self.hp -= dmg
@@ -270,7 +52,58 @@ class Bot:
                 score += 10
             self.isDead = True
 
-    def animate(self):
+    def collide(self, obj):
+        if obj.isDead:
+            return
+
+        def isInRect(x, y, obj):
+            return obj.x <= x <= obj.x + obj.width and obj.y <= y <= obj.y + obj.height
+
+        res = isInRect(self.x, self.y, obj)
+        res |= isInRect(self.x + self.width, self.y, obj)
+        res |= isInRect(self.x + self.width, self.y + self.height, obj)
+        res |= isInRect(self.x, self.y + self.height, obj)
+        return res
+
+    def attack(self, cnd1, cnd2):
+        if not self.isAttacking:
+            if cnd1:
+                self.attackCount = 0
+                self.attackType = 0
+                self.isAttacking = True
+        else:
+            if self.attackCount == 0:
+                sound['punch'].play()
+            self.attackCount += 1
+            if self.attackCount == 4:
+                for obj in objects:
+                    if obj.friendly != self.friendly and self.collide(obj):
+                        obj.damage(self.dmg * (self.attackType + 1))
+            if self.attackCount == 8:
+                if cnd2:
+                    self.attackType += 1
+                    if (self.attackType == 3):
+                        self.attackType = 0
+                        self.isAttacking = False
+                    self.attackCount = 0
+                else:
+                    self.isAttacking = False
+                    self.attackCount = 0
+                    self.attackType = 0
+
+    def jump(self, cnd):
+        if not self.isJump:
+            if cnd:
+                self.isJump = True
+        else:
+            if self.jumpCount >= -10:
+                self.y -= 2 * self.jumpCount
+                self.jumpCount -= 1
+            else:
+                self.jumpCount = 10
+                self.isJump = False
+
+    def animate_char(self):
         global win
         if self.isDead:
             win.blit(self.pictures['dead{}'.format('L' if self.isLeft else 'R')]
@@ -299,6 +132,107 @@ class Bot:
                      [self.animCount // 5], (self.x, self.y))
             self.animCount += 1
             self.animCount %= 30
+
+class Hero(Character):
+    def __init__(self, pictures, x=50, friendly=True):
+        super().__init__(pictures, x, friendly, 300, 8)
+        self.dmg = 10
+        self.cp = 2000
+
+    def shadow_clone_jutsu(self):
+        if self.cp <= 1500:
+            return
+        amount = random.randint(1, 4)
+        self.cp //= (amount + 1)
+        global narutoImg
+        pygame.mixer.Sound('sound/shadow_clone_jutsu.wav').play()
+        for i in range(amount):
+            objects.append(Bot(narutoImg, friendly=True, x=self.x, lvl=2))
+
+    def act(self):
+        if self.isDead:
+            return
+
+        self.cp += 1 if self.cp < 3000 else 0
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_k]:
+            self.shadow_clone_jutsu()
+
+        cnd1 = cnd2 = keys[pygame.K_j]
+        self.attack(cnd1, cnd2)
+
+        cnd1 = keys[pygame.K_a] and self.x > 5
+        cnd2 = keys[pygame.K_d] and self.x < sizeX - self.width - 5
+        self.isIdle = not (cnd1 or cnd2)
+        self.isLeft = cnd1 if not self.isIdle else self.isLeft
+        self.x += (0 if self.isIdle else 1) * (-1 if cnd1 else 1) * self.speed
+
+        cnd = keys[pygame.K_SPACE]
+        self.jump(cnd)
+
+    def animate(self):
+        global win
+        win.blit(self.pictures['prof'][0], (20, 20))
+        pygame.draw.rect(win, (255, 0, 0), (90, 43, 300, 10))
+        pygame.draw.rect(win, (0, 200, 255), (90, 53, self.cp // 10, 10))
+        pygame.draw.rect(win, (0, 0, 0), (90, 53, self.cp // 10, 10), 1)
+        if not self.isDead:
+            pygame.draw.rect(win, (0, 255, 0), (90, 43, self.hp, 10))
+            pygame.draw.rect(win, (0, 0, 0), (90, 43, self.hp, 10), 1)
+        pygame.draw.rect(win, (0, 0, 0), (90, 43, 300, 10), 1)
+        pygame.draw.rect(win, (0, 0, 0), (90, 53, 300, 10), 1)
+        self.animate_char()
+
+
+class Bot(Character):
+    def __init__(self, pictures, friendly=False, lvl=1, x = 'nt'):
+        self.lvl = lvl
+        self.dmg = 5 * lvl
+        self.isTargetChosen = False
+        x = (sizeX // 2) + (2 * random.randint(0, 1) - 1) * (
+                sizeX // 2 + 50) if x == 'nt' else x
+        super().__init__(pictures, x, friendly, 10 + 20 * lvl, 4 if lvl < 5 else 6)
+
+    def chooseTarget(self):
+        if self.isTargetChosen:
+            if self.target.isDead:
+                self.isTargetChosen = False
+            return
+        enemies = []
+        for obj in objects:
+            if self.friendly != obj.friendly and not obj.isDead:
+                enemies.append(obj)
+        if len(enemies):
+            self.isTargetChosen = True
+            self.target = enemies[random.randint(0, len(enemies) - 1)]
+
+    def act(self):
+        if self.isDead:
+            return
+
+        self.chooseTarget()
+
+        at_prob = random.randint(1, 200) <= 9
+        cnd2 = self.isTargetChosen and self.collide(self.target)
+        cnd1 = cnd2 and at_prob
+        self.attack(cnd1, cnd2)
+
+        if not self.isTargetChosen:
+            self.isIdle = True
+            return
+
+        self.isLeft = (self.x > self.target.x)
+        self.isIdle = self.collide(self.target)
+        self.x += (-1 if self.isLeft else 1) * (0 if self.isIdle else 1) * (
+            self.speed + 2 * random.randint(0, 1) - 1)
+
+        jumpProb = random.randint(1, 200) <= 3
+        cnd = self.isTargetChosen and not self.isJump and jumpProb
+        self.jump(cnd)
+
+    def animate(self):
+        self.animate_char()
 
 
 narutoImg = {'runR': [], 'runL': [], 'idleR': [], 'idleL': [],
@@ -340,7 +274,7 @@ def DrawWindow(objects):
         obj.animate()
     win.blit(usedFont.render('Score: ' + str(score), 0, (0, 0, 0)), (20, 100))
     if objects[0].isDead:
-        win.blit(usedFont.render('GAME OVER! Press ESC to quit', 0, (0, 0, 0)), (100, 90))
+        win.blit(usedFont.render('GAME OVER! Press ESC to quit', 0, (0, 0, 0)), (350, 90))
     pygame.display.update()
 
 
